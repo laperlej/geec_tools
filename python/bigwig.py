60b46526372930b7c77bdd7f204ab139694371c7
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-03-23 13:41:39 -0400

    bugfix

--- python/bigwig.py
+++ python/bigwig.py
@@ -24,7 +24,8 @@ class BigWig(object):
 
     def symlink(self):
         #TODO do away with tempnam
-        new_path = os.tempnam(config.TMP_DIR)
+        #new_path = os.tempnam(config.TMP_DIR)
+        new_path = os.path.join(os.path.dirname(self.path), os.path.basename(self.path).replace(':','_'))
         os.symlink(self.path, new_path)
         return new_path
 
