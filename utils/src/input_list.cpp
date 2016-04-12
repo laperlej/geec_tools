ac513abe80a66cdf2a7ab42ffdbede9e279e2add
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-04-12 16:06:27 -0400

    prevent duplicate names in input_list

--- utils/src/input_list.cpp
+++ utils/src/input_list.cpp
@@ -1,4 +1,4 @@
-//
+fast check for duplicates //
 //  file_list.cpp
 //  GeFF
 //
@@ -14,14 +14,19 @@
 InputList::InputList(const std::string& file_path) {
   std::ifstream flot(file_path);
   std::string path, name;
+  int count = 0;
+  bool exists = 0;
   while (flot>> path>> name) {
-    files_.push_back(path);
-    names_.push_back(name);
+    exists = index_map_.emplace(name, count) -> second; // ensures no duplicates
+    if (!exists) {
+      files_.push_back(std::make_pair(path, name));
+      ++count;
+    }
   }
 }
 
 std::pair<std::string, std::string> InputList::operator[](const int index) {
-  return std::make_pair(files_[index], names_[index]);
+  return files_[index];
 }
 
 size_t InputList::size() {
