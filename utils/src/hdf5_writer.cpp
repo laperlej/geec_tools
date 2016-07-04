f727cf30e76d0dcfde4464ddea34db55b1d11929
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-07-04 19:58:52 -0400

    submission for debug

--- utils/src/hdf5_writer.cpp
+++ utils/src/hdf5_writer.cpp
@@ -60,7 +60,7 @@ void Hdf5Writer::AddDataset(Hdf5Dataset& hdf5_dataset) {
 }
 
 void Hdf5Writer::AddGenomicDataset(GenomicDataset& genomic_dataset) {
-  for (std::pair<const std::string, Hdf5Dataset> chrom: genomic_dataset) {
+  for (std::pair<const std::string, Hdf5Dataset> chrom: genomic_dataset.chromosomes()) {
     AddDataset(chrom.second)
   }
 }
