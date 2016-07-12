81549a892add5684f9e7cc00b081c51c39dd0ec9
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-07-12 10:35:40 -0400

    bugfix

--- utils/src/hdf5_dataset.cpp
+++ utils/src/hdf5_dataset.cpp
@@ -139,10 +139,10 @@ float Hdf5Dataset::GetPearson(Hdf5Dataset& hdf5_dataset) {
   //std::cout << num << std::endl;
   std::cout << pow(sumX_, 2) << std::endl;
   std::cout << pow(sumY, 2) << std::endl;
+  std::cout << pow(sumXX_ - pow(sumX_, 2), 0.5) << std::endl;
+  std::cout << pow(sumYY - pow(sumY, 2), 0.5) << std::endl;
   std::cout << denum << std::endl;
-  std::cout << denum << std::endl;
-  std::cout << denum << std::endl;
-  std::cout << denum << std::endl;
+  
   r = num / denum;
   return r;
 }
