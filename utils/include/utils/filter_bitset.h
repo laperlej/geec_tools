593646fc76b6be3a3a324c58a269a4c3eaf10569
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-07-11 21:01:16 -0400

    bugfix

--- utils/include/utils/filter_bitset.h
+++ utils/include/utils/filter_bitset.h
@@ -45,6 +45,7 @@ class FilterBitset {
       start_bin = token.start_position() / bin_;
       end_bin = token.end_position() / bin_;
       for (int i = start_bin; i <= end_bin; ++i) {
+        std::cout << chrom << " : " << i << std::endl;
         content_[chrom][i] = 1;
       }
     }
