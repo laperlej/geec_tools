01655685c294a4789d6199427bc65cb75268644a
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-08-15 15:59:33 -0400

    added attributes to hdf5

--- utils/include/utils/utils.h
+++ utils/include/utils/utils.h
@@ -3,11 +3,16 @@
 #include <iostream>
 
 std::string md5sum(std::string file_path) {
+  std::cout << md5sum(chrom_path)<< std::endl;
   std::ifstream file(file_path);
-  std::basic_string<unsigned char> md5;
-  md5.reserve(32);
+  std::basic_string<unsigned char> digest;
+  digest.reserve(16);
   std::basic_string<unsigned char> content((std::istreambuf_iterator<char>(file)),
                  std::istreambuf_iterator<char>());
-  md5 = MD5(&content[0], content.size(), &md5[0]);
-  return (char*)&md5[0];
+  md5 = MD5(&content[0], content.size(), &digest[0]);
+  std::string md5;
+  md5.reserve(32);
+  for(int i = 0; i < 16; ++i)
+    sprintf(&md5[i*2], "%02x", (unsigned int)digest[i]);
+  return md5;
 }
