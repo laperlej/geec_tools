8c0679b4e245df0cd90c8e0cda525b5c167e1b69
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-08-18 20:53:47 -0400

    bigfix

--- geec_filter.sh
+++ geec_filter.sh
@@ -16,8 +16,8 @@ filtered_commands=$(mktemp)
 
 while IFS=$'\n' read -r line; do
 	IFS=$'\t' read -r bw_file name hdf5_file filter_file <<< "$line"
-	echo "$to_hdf5 $bw_file $name $chrom_sizes $hdf5_file $bin" > hdf5_commands
-	echo "$filter $hdf5_file $name $filter_file $chrom_sizes $bin $include $exclude" > filtered_commands
+	echo "$to_hdf5 $bw_file $name $chrom_sizes $hdf5_file $bin" >> hdf5_commands
+	echo "$filter $hdf5_file $name $filter_file $chrom_sizes $bin $include $exclude" >> filtered_commands
 done < "$file_list"
 
 cat $hdf5_commands | xargs -I CMD -P 24 bash -c CMD
