38ab58e74714e4168a4afff00b66a5ec3a8f72aa
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-08-27 22:55:48 -0400

    bugfix

--- python/geec.py
+++ python/geec.py
@@ -39,9 +39,9 @@ def filter_hdf5(name, chrom_sizes, user_hdf5, filtered_hdf5, resolution, include
                     ])
 
 def process_unit(args):
-	raw_file, name, chrom_sizes, user_hdf5, filtered_hdf5, resolution, include, exclude = args
-	to_hdf5(raw_file, name, chrom_sizes, user_hdf5, resolution)
-	filter_hdf5(name, chrom_sizes, user_hdf5, filtered_hdf5, resolution, include, exclude)
+    raw_file, name, chrom_sizes, user_hdf5, filtered_hdf5, resolution, include, exclude = args
+    to_hdf5(raw_file, name, chrom_sizes, user_hdf5, resolution)
+    filter_hdf5(name, chrom_sizes, user_hdf5, filtered_hdf5, resolution, include, exclude)
 
 def correlate(input_list, chrom_sizes, correlation_file, resolution):
     """Usage: correlation {input_list}
@@ -90,26 +90,26 @@ def tmp_name():
     return temp_path
 
 def main():
-	resolution = "10000"
-	assembly = "hg19"
-	include = "/mnt/parallel_scratch_mp2_wipe_on_august_2016/jacques/laperlej/geec_tools/resource/region/hg19.all.bed"
-	exclude = "/mnt/parallel_scratch_mp2_wipe_on_august_2016/jacques/laperlej/geec_tools/resource/region/hg19.exclude.bed"
-	chrom_sizes = "/mnt/parallel_scratch_mp2_wipe_on_august_2016/jacques/laperlej/geec_tools/resource/chrom_sizes/hg19noY.chrom.sizes"
+    resolution = "10000"
+    assembly = "hg19"
+    include = "/mnt/parallel_scratch_mp2_wipe_on_august_2016/jacques/laperlej/geec_tools/resource/region/hg19.all.bed"
+    exclude = "/mnt/parallel_scratch_mp2_wipe_on_august_2016/jacques/laperlej/geec_tools/resource/region/hg19.exclude.bed"
+    chrom_sizes = "/mnt/parallel_scratch_mp2_wipe_on_august_2016/jacques/laperlej/geec_tools/resource/chrom_sizes/hg19noY.chrom.sizes"
 
-	list_path = sys.argv[1]
-	args_list=[]
-	with open(list_path, 'r') as list_file:
-		for line in list_file:
-			line = line.split()
-			raw_file = line[0]
-			name = line[1]
-			user_hdf5 = line[2]
-			filtered_hdf5 = line[3]
-			args = (raw_file, name, chrom_sizes, user_hdf5, filtered_hdf5, resolution, include, exclude)
-			args_list.append(args)
-	pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    list_path = sys.argv[1]
+    args_list=[]
+    with open(list_path, 'r') as list_file:
+        for line in list_file:
+            line = line.split()
+            raw_file = line[0]
+            name = line[1]
+            user_hdf5 = line[2]
+            filtered_hdf5 = line[3]
+            args = (raw_file, name, chrom_sizes, user_hdf5, filtered_hdf5, resolution, include, exclude)
+            args_list.append(args)
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
     try:
-	   pool.map(process_unit, args_list)
+       pool.map(process_unit, args_list)
     except KeyboardInterrupt:
         pool.terminate()
         exit(1)
