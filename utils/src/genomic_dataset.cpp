38de27ff524965c91e41d26ef58d81266c35223e
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2017-01-09 17:26:26 -0500

    bugfix

--- utils/src/genomic_dataset.cpp
+++ utils/src/genomic_dataset.cpp
@@ -29,7 +29,7 @@ std::string GenomicDataset::get_name() {
 std::string GenomicDataset::get_sizes() {
   std::string sizes = "";
   for (const std::string& chr : chromosomes_) {
-    sizes += chr + ":" + chromosomes_[chr].size();
+    sizes += chr + ":" + chromosomes_.find(chr).size();
   }
   return sizes;
 }
