da292fa723254f51a99f0dafb103c1f00f4e7c20
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2017-02-13 10:45:21 -0500

    fixed correlation wrapper

--- python/geec_corr.py
+++ python/geec_corr.py
@@ -15,13 +15,22 @@ def tmp_name():
     os.remove(temp_path)
     return temp_path
 
-def correlation(input_path, corr_path, mat_path):
+def correlation(input_file, corr_path, mat_path):
+    assembly = geec_config.ASSEMBLY
     include = config.get_region(assembly, geec_config.INCLUDE)
     exclude = config.get_region(assembly, geec_config.EXCLUDE)
-    assembly = geec_config.ASSEMBLY
     resolution = geec_config.RESOLUTION
     chrom_sizes = config.get_chrom_sizes(assembly)
 
+    input_path = tmp_name()
+    corr_input_file = open(input_path)
+    for line in input_file:
+        name = line.strip()
+        filtered_name = "{0}_{1}_{2}_{3}.hdf5".format(name, config.get_resolution(resolution), geec_config.INCLUDE, geec_config.EXCLUDE)
+        filtered_hdf5 = os.path.join(geec_config.FILTERED_FOLDER, filtered_name)
+        corr_input_file.write("{0}\t{1}\n".format(filtered_hdf5, name))
+    corr_input_file.close()
+    
     correlate(input_path, chrom_sizes, corr_path, resolution)
     make_matrix(input_path, chrom_sizes, corr_path, mat_path)
     
@@ -29,7 +38,7 @@ def main():
     list_path = sys.argv[1]
     corr_path = sys.argv[2]
     mat_path = sys.argv[3]
-    correlation(list_path, corr_path, mat_path)
+    correlation(open(list_path), corr_path, mat_path)
 
 if __name__ == '__main__':
     main()
