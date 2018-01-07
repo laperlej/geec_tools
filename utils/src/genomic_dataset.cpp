b83c9440780a5b211c921cdcc327ec8bfc643ae3
laperlej <jonathan.laperle@usherbrooke.ca>
2018-01-07 13:04:18 -0500

    debug

--- utils/src/genomic_dataset.cpp
+++ utils/src/genomic_dataset.cpp
@@ -45,7 +45,7 @@ std::map<std::string, float>  GenomicDataset::Correlate(
       float r;
       try{
         r = chromosomes_.at(chr)->GetPearson(
-        genomic_dataset.chromosomes().at(chr));
+        *(genomic_dataset.chromosomes().at(chr)));
       } catch (...) {
         r = 0;
       }
